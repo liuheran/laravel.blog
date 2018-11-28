8ddd76400762776a35faee4207a4668e5ef34aa3
liuheran <liuheran@ihangmei.com>
2018-11-28 18:10:26 +0800

    modify!

--- homestead_categories.sql
+++ homestead_categories.sql
@@ -6,14 +6,14 @@ CREATE TABLE homestead.categories
     created_at timestamp,
     updated_at timestamp
 );
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (1, 'PHP', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (2, 'MySQL', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (3, 'Linux', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (4, 'Redis', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (5, 'JavaScript', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (6, 'HTML5/CSS3', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (7, '算法', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (8, '数据结构', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (9, '设计模式', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (10, 'Docker', 100, null, null);
-INSERT INTO homestead.categories (id, name, sort, created_at, updated_at) VALUES (11, '其它', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (1, 'PHP', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (2, 'MySQL', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (3, 'Linux', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (4, 'Redis', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (5, 'JavaScript', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (6, 'HTML5/CSS3', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (7, '算法', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (8, '数据结构', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (9, '设计模式', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (10, 'Docker', 100, null, null);
+INSERT INTO categories (id, name, sort, created_at, updated_at) VALUES (11, '其它', 100, null, null);
